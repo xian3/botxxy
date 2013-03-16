27822a208a4f33c9d633b0eb0d1885009f6b6a0d
Josh <noreply@gmail.com>
2013-03-16 00:41:09 -0400

    major changes

--- src/BaseBot.py
+++ src/BaseBot.py
@@ -1,47 +1,52 @@
-# https://en.wikipedia.org/wiki/List_of_Internet_Relay_Chat_commands
-# http://wiki.shellium.org/w/Writing_an_IRC_bot_in_Python
-# http://forum.codecall.net/topic/59608-developing-a-basic-irc-bot-with-python/
-# http://docs.python.org/2/library/ssl.html
-# http://docs.python.org/2/library/hashlib.html
-# https://www.hackthissite.org/articles/read/1050
-# http://stackoverflow.com/questions/4719438/editing-specific-line-in-text-file-in-python
-
-# Import the necessary libraries.
 import socket
 import ssl
-#import hashlib
 import time
 import random
+
+debug = True
 
 class BaseIRC(object):
 	def __init__(self, *args, **kwargs):
 		self.server = kwargs.get("server", "boxxybabee.catiechat.net")
 		self.port = kwargs.get("port", 6667)
+		self.ssl_port = kwargs.get("port", 6697)
 		self.useSSL = kwargs.get("useSSL", False)
 		self.info_user = kwargs.get("info_user", "I")
 		self.info_host = kwargs.get("info_host", "m.botxxy.you.see")
 		self.info_server = kwargs.get("info_server", "testserver")
 		self.info_name = kwargs.get("info_name", "testname")
+		self.info_name = kwargs.get("info_name", "ircFemBot")
 		self.ircsock = None
 		self.running = False
 	def connect(self):
 		try:
 			self.ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			if self.useSSL: self.ircsock = ssl.wrap_socket(ircsock)
+			if self.useSSL: self.ircsock = ssl.wrap_socket(self.ircsock)
 			self.ircsock.connect((self.server, self.ssl_port))
-		except SocketError:
+		except socket.error:
 			return False
 		return True
-	def dispatch(self):
+	def dispatchCommand(self):
 		raise NotImplemented
 	def send(self, msg):
-		print(msg)
-		#self.ircsock.send(msg)
+		if debug: print(msg)
+		else:
+			print(msg)
+			self.ircsock.send(msg)
+	def recv(self):
+		if debug: return raw_input(">>>")
+		else:
+			try:
+				recvd = self.ircsock.recv(512)
+			except KeyboardInterrupt:
+				raise KeyboardInterrupt
+			print(recvd)
+			return recvd
 	def server_login(self):	
-		self.send("USER %s %s %s %s\n" % ( botuser, bothost, botserver, botname) )
-		self.send("NICK %s\n" % botnick) # Here we actually assign the nick to the bot
+		self.send("USER %s %s %s %s\n" % ( self.info_user, self.info_host, self.info_server, self.info_name) )
+		self.send("NICK %s\n" % self.info_nick) # Here we actually assign the nick to the bot
 		time.sleep(3)
-		self.send("NICKSERV IDENTIFY %s\n" % botpassword) # Identifies the bot's nickname with nickserv
+		self.send("NICKSERV IDENTIFY %s\n" % "temporary") # Identifies the bot's nickname with nickserv
 		time.sleep(3)
 		self.send("NICKSERV SET ENFORCE ON\n")
 	#
@@ -49,57 +54,72 @@ class BaseIRC(object):
 		self.send("JOIN %s\n" % channel)
 	def leaveChannel(self, channel):
 		self.send("LEAVE %s\n" % channel)
+	def changeTopic(self, channel, topic):
+		self.send("TOPIC %s :%s\n" % ( channel, topic ) )
+	def invite(self, nick, channel): # Invites given nickname to present channel
+		self.send("INVITE %s %s\n" % ( nick, channel ) )
+	def kick(self, nick, channel, msg='lol butthurt\n'):
+		self.send("KICK %s %s %s\n" % (channel, nick, msg))
+	def voice(self, nick,channel, mode='+'):
+		self.send("MODE %s %sv %s\n" % ( channel, mode[0], nick) )
+	def op(self, nick,channel, mode='+'):
+		self.send("MODE %s %so %s\n" % ( channel, mode[0], nick) )
+	def hop(self, nick,channel, mode='+'):
+		self.send("MODE %s %sh %s\n" % ( channel, mode[0], nick) )
 	#
 	def sendChanMsg(self, channel, msg): # This sends a message to the channel 'chan'
 		self.send("PRIVMSG %s :%s\n" % ( channel, msg ) )
 	def sendUserMsg(self, nick, msg): # This sends a notice to the nickname 'nick'
 		self.send("NOTICE %s :%s\n" % ( nick, msg ) )
-		
-	def getNick(self, msg): # Returns the nickname of whoever requested a command from a RAW irc privmsg. Example in commentary below.
-		# ":b0nk!LoC@fake.dimension PRIVMSG #test :lolmessage"
-		return msg[msg.index(':')+1:msg.index('!')]
-	def getUser(self, msg): # Returns the user and host of whoever requested a command from a RAW irc privmsg. Example in commentary below.
-		# ":b0nk!LoC@fake.dimension PRIVMSG #test :lolmessage"
-		return msg[msg.index(':')+1:msg.index(' PRIVMSG ')]
-	def getChannel(self, msg): # Returns the channel from whereever a command was requested from a RAW irc PRIVMSG. Example in commentary below.
-		# ":b0nk!LoC@fake.dimension PRIVMSG #test :lolmessage"
-		return msg.index(' PRIVMSG ')[-1].split(' :')[0]
-	
-	def ping(self, reply): # This is our first function! It will respond to server Pings.
-		self.send("PONG :%s\n" % reply)
-	def hello(self, msg): # This function responds to a user that inputs "Hello testbot"
-		nick = getNick(msg)
-		chan = getChannel(msg)
-		self.send("PRIVMSG %s :Hello %s! Type !help for more information.\n" % ( chan, nick ) )
+
+	def ping(self, msg): # This is our first function! It will respond to server Pings.
+		self.send("PING :%s\n" % msg)	
+	def pong(self, msg="Botxxy pew pew"): # This is our first function! It will respond to server Pings.
+		self.send("PONG :%s\n" % msg)
+
 
 class BotIRC(BaseIRC):
 	def __init__(self, *args, **kwargs):
+		super(BotIRC, self).__init__(*args, **kwargs)
 		print('init BotIRC')
-		super(BaseIRC, self).__init__(*args, **kwargs)
-		self.commandsList = {}
+		self.running = False
+		self.userCommands = {}
+		self.serverCommands = {}
+		#self.connect()
 	def registerCommand(self, string, command):
-		print('registering \'%s\'' %string)
 		if type(string) is str:
-			self.commandsList[string] = command
+			if string.startswith('!'): #USER command
+				if debug: 	print('registering user command: \'%s\'' %string)
+				self.userCommands[string] = command
+			else:
+				if debug: 	print('registering server command: \'%s\'' %string)
+				self.serverCommands[string] = command
 	def unregisterCommand(self, string):
-		print('unregistering \'%s\'' %string)
-		del self.commandsList[string]
+		print('unregistering \'%s\'' %handlerPrefix)
+		if string.startswith(' '): #SERVER command
+			del self.serverCommands[handlerPrefix]
+		elif string.startswith('!'): #USER command
+			del self.userCommands[handlerPrefix]
 	def dispatchCommand(self, msg):
-		for handlerPrefix in self.commandsList.keys():
-			if msg.startswith(handlerPrefix):
-				print('dispatching \'%s\'' % handlerPrefix)
-				self.commandsList[handlerPrefix](msg)
-
-def main():
-	b = BotIRC()
-	def haha(msg):
-		print('haha %s' % msg)
-	b.registerCommand(' 359 ', haha)
-	print(b.commandsList)
-	msg = ' 359 this is a test'
-	b.dispatchCommand(msg)
-	b.unregisterCommand(' 359 ')
-	print(b.commandsList)
-	
-if __name__ == '__main__':
-	main()
+		if msg.startswith(':') and msg.count(':') > 2: #USER command
+			print msg[msg.index(':',1)+1:]
+			
+			for handlerPrefix in self.userCommands.keys():
+				if msg[msg.index(':',1)+1:].startswith(handlerPrefix):
+					if debug: 	print('dispatching user command: \'%s\'' %handlerPrefix)
+					self.userCommands[handlerPrefix](msg)
+		else:
+			for handlerPrefix in self.serverCommands.keys():
+				if msg.startswith(handlerPrefix):
+					if debug: 	print('dispatching server command: \'%s\'' %handlerPrefix)
+					self.serverCommands[handlerPrefix](msg)
+	def run(self):
+		self.running = True
+		while self.running:
+			try:
+				self.dispatchCommand(self.recv())
+			except KeyboardInterrupt:
+				self.running = False
+				
+				
+				
